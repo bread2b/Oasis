83a4a26466b5cf6a0db2c6332c31ad58a5e5ef49
Haichuan Wang <hwa192@sfu.ca>
2025-12-22 19:16:59 -0800

    a

---
+++ core/assets/tilesets/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="house" tilewidth="16" tileheight="16" tilecount="3844" columns="62">
+ <image source="house.png" width="1000" height="1000"/>
+</tileset>
