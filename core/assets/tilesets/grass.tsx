494b945fae738b8abc3e572106264783f6f95371
Haichuan Wang <hwa192@sfu.ca>
2025-12-23 15:38:17 -0800

    a

--- core/assets/tilesets/grass.tsx
+++ core/assets/tilesets/grass.tsx
@@ -1,4 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="grass" tilewidth="16" tileheight="16" tilecount="961" columns="31">
  <image source="grass.png" width="500" height="500"/>
+ <wangsets>
+  <wangset name="beach" type="corner" tile="-1">
+   <wangcolor name="water" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="270" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="272" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="302" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="332" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="334" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="391" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="392" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="393" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="422" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="424" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="453" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="454" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="455" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
---
+++ core/assets/tilesets/sample.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sample" tilewidth="16" tileheight="16" tilecount="1728" columns="48">
+ <image source="sample.png" width="768" height="576"/>
+ <wangsets>
+  <wangset name="watergrass" type="mixed" tile="-1">
+   <wangcolor name="water" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="shore" type="corner" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="17" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="64" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="111" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="113" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="156" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="157" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="158" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="159" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="160" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="161" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="204" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="209" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="252" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="257" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="300" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="305" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="348" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="353" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="396" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="397" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="398" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="399" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="400" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="401" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
