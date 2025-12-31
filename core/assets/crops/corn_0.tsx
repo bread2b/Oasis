823969c47080eacf2871522c2cbb24bdc8bfcd3a
Wang <whc2004221@163.com>
2025-12-30 17:32:57 -0800

    a

--- core/assets/crops/corn_0.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="corn_0" tilewidth="16" tileheight="16" tilecount="2" columns="1">
- <image source="corn_0.png" width="16" height="32"/>
-</tileset>
